c58a286988779821a2d42df7b34ecb55854cedab
Philip McMahon <philip.mcmahon@guardian.co.uk>
2023-03-14 13:06:07 +0000

    Launch whistleflow after launching securedrop client

---
+++ launcher/whistleflow_launcher.py
@@ -0,0 +1,12 @@
+from sdw_util import Util
+import subprocess
+
+logger = Util.get_logger(module=__name__)
+
+
+def launch_whistleflow():
+    """Launch the Whistleflow encrypt and send services"""
+    logger.info("Launching whistleflow-encrypt")
+    subprocess.Popen(["qvm-run", "whistleflow-encrypt", "systemctl start whistleflow-encrypt"])
+    logger.info("Launching whistleflow-send")
+    subprocess.Popen(["qvm-run", "whistleflow-send", "systemctl start whistleflow-send"])
