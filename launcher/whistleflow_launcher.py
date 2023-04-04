37c4793ad77b5ded69c851194f2dc664f10c0bf6
Philip McMahon <philip.mcmahon@guardian.co.uk>
2023-04-04 17:58:56 +0100

    Launch whistleflow-view as well

--- launcher/whistleflow_launcher.py
+++ launcher/whistleflow_launcher.py
@@ -5,7 +5,9 @@ logger = Util.get_logger(module=__name__)
 
 
 def launch_whistleflow():
-    """Launch the Whistleflow encrypt and send services"""
+    """Launch the Whistleflow view, encrypt and send services"""
+    logger.info("Launching whistleflow-view")
+    subprocess.Popen(["qvm-run", "whistleflow-view", "systemctl start whistleflow-view"])
     logger.info("Launching whistleflow-encrypt")
     subprocess.Popen(["qvm-run", "whistleflow-encrypt", "systemctl start whistleflow-encrypt"])
     logger.info("Launching whistleflow-send")
