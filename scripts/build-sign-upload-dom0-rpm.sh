ee5bc698bf5875f4d94abf123c789c5443a923d1
philmcmahon <philip.mcmahon@theguardian.com>
2023-04-12 10:35:21 +0100

    Use docker for rpm building/signing to remove the need for a separate build server

--- scripts/build-sign-upload-dom0-rpm.sh
+++ scripts/build-sign-upload-dom0-rpm.sh
@@ -3,22 +3,43 @@ set -e
 # This script calls out to the existing build-rpm script then signs the resulting file with the guardian's private key, and uploads it to S3
 
 SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
+export AWS_PROFILE=investigations
+export AWS_DEFAULT_REGION=eu-west-1
 
-$SCRIPT_PATH/build-dom0-rpm
+STAGE=${1:-"CODE"}
 
-# Fetch key from secrets manager and import into temporary gpg keyring
-aws secretsmanager get-secret-value --region eu-west-1 --secret-id "$SIGNING_KEY_SECRET_ID" | jq .SecretString -r > /home/admin/private.asc
+if [[ $STAGE != 'CODE' && $STAGE != 'PROD' ]]; then
+  echo "Stage must be CODE or PROD (case sensitive)"
+  exit 1
+fi
 
-KEYRING="gu-securedrop-temporary.gpg" # this must correspond to value set in /home/admin/.rpmmacros
-gpg --no-default-keyring  --keyring $KEYRING  --pinentry loopback --import /home/admin/private.asc
+mkdir -p /tmp/sdw
+
+
+DOCKER_BASE_COMMAND="docker run -v $SCRIPT_PATH/../:/src -v /tmp/sdw:/tmp "
+DOCKER_RUN_COMMAND="sdwbuild /bin/sh -c "
+
+# Build docker container
+docker build -t sdwbuild $SCRIPT_PATH/../rpm-build-docker
+
+# Generate RPM
+$DOCKER_BASE_COMMAND $DOCKER_RUN_COMMAND 'cd /src; scripts/build-dom0-rpm'
+
+# Fetch key from secrets manager
+SIGNING_KEY_SECRET_ID=$(aws ssm get-parameter --name /$STAGE/investigations/securedrop-workstation/signingKeySecretId | jq -r .Parameter.Value)
+aws secretsmanager get-secret-value --region eu-west-1 --secret-id "$SIGNING_KEY_SECRET_ID" | jq .SecretString -r > /tmp/sdw/private.asc
 
 # Sign RPM
-LATEST_RPM_PATH="$(find $SCRIPT_PATH/../rpm-build/ -type f -iname '*.rpm' -print0 | sort -zV | head -n 1 )"
-rpm --addsign "$LATEST_RPM_PATH"
+echo "Signing RPM..."
+$DOCKER_BASE_COMMAND -it $DOCKER_RUN_COMMAND /src/scripts/sign-rpm.sh $STAGE
 
+echo "Uploading signed RPM..."
+LATEST_RPM_PATH=$(find /$SCRIPT_PATH/../rpm-build/ -type f -iname '*.rpm' -print0 | sort -zV | head -n 1 )
 LATEST_RPM_FILENAME="$(basename "$LATEST_RPM_PATH")"
 
 # Upload
-aws s3 cp "$LATEST_RPM_PATH" s3://$WORKSTATION_RELEASE_BUCKET/$LATEST_RPM_FILENAME
+RELEASE_BUCKET=$(aws ssm get-parameter --name /$STAGE/investigations/securedrop-workstation/releaseBucket | jq -r .Parameter.Value)
+aws s3 cp "$LATEST_RPM_PATH" s3://$RELEASE_BUCKET/$LATEST_RPM_FILENAME
 
-rm ~/.gnupg/$KEYRING
+# Tidy up temp files
+rm /tmp/private.asc
